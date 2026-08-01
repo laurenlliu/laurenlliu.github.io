fc9ff96b1e19275fb189975489c11719f9feaaf2
Lauren <laasliu@ucsc.edu>
2026-08-01 01:32:54 -0700

    Add a console greeting for recruiters who open devtools
    A styled console.log on every page (not the 404) with a friendly
    message and contact info, for the segment of visitors who actually
    pop the console open.

---
+++ js/console-greeting.js
@@ -0,0 +1,12 @@
+console.log(
+  '%c🐷 hi, curious engineer.',
+  'font-size: 16px; font-weight: 600; color: #C2185B;'
+);
+console.log(
+  '%cLike what you see under the hood? Let\'s talk: llaurenlliu@gmail.com',
+  'font-size: 13px; color: #146356;'
+);
+console.log(
+  '%cgithub.com/laurenlliu · linkedin.com/in/laurenlliu',
+  'font-size: 12px; color: #7A5866;'
+);
